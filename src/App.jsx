b064fdd4cfac4c02398925a041709b0668352e51
Hrithik <slumdog4321@gmail.com>
2024-03-05 00:06:52 +0530

    some  error occured

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import { Button } from "@/components/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1 className="font-bold text-4xl text-center">Welcome to reality</h1>
